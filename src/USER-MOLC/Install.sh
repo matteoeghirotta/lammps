6ee910d26f45d69f4ea0f8e1b60f2c477da6ab3e
Matteo Ricci <matteo.ricci@studiomado.it>
2020-04-28 13:04:46 +0200

    Install.sh (check)

---
+++ src/USER-MOLC/Install.sh
@@ -0,0 +1,38 @@
+# Install/unInstall package files in LAMMPS
+# mode = 0/1/2 for uninstall/install/update
+
+mode=$1
+
+# arg1 = file, arg2 = file it depends on
+
+# enforce using portable C locale
+LC_ALL=C
+export LC_ALL
+
+action () {
+  if (test $mode = 0) then
+    rm -f ../$1
+  elif (! cmp -s $1 ../$1) then
+    if (test -z "$2" || test -e ../$2) then
+      cp $1 ..
+      if (test $mode = 2) then
+        echo "  updating src/$1"
+      fi
+    fi
+  elif (test -n "$2") then
+    if (test ! -e ../$2) then
+      rm -f ../$1
+    fi
+  fi
+}
+
+# list of files with dependcies
+
+action bond_ellipsoid.h atom_vec_ellipsoid.h
+action bond_ellipsoid.h molecule.h
+action bond_ellipsoid.cpp atom_vec_ellipsoid.h
+action bond_ellipsoid.cpp molecule.h
+action bond_molc.h
+action bond_molc.cpp
+action pppm_offcentre.h
+action pppm_offcentre.cpp atom_vec_ellipsoid.h
